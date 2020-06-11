1b781cd19b71b3ecbb54d147e83965519c2b2c07
it0sat0 <wild_hosikuzu@icloud.com>
2020-06-11 22:40:17 +0900

    river_builder fix

--- river_builder.py
+++ river_builder.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 from functions import *
-
+from test import *
 
 class River_Builder:
     # if road_type is 1(sand&river), depth is only 16
 
-    def __init__(self, level, start_x, start_y, start_z, width, depth, direction):
+    def __init__(self, level, start_x, start_y, start_z, width, height, depth, direction):
         self.level = level
         self.start_x = start_x
         self.start_y = start_y
         self.start_z = start_z
         self.width = width
+        self.height = height
+        self.depth = depth
         self.direction = direction
 
     def build(self):
@@ -20,24 +22,37 @@ class River_Builder:
         y = self.start_y
         z = self.start_z
         w = self.width
+        h = self.height
         d = self.depth
-        if self.direction is 0: 
-            for j in range(d): 
-                if (j==0 or j==d):
-                    for k in range(4):
-                        setBlock(lv, x+j, y-k, z+i, 1, 0)
-                else:
-                    setBlock(lv, x+j, y-k, z+i, 0, 0)
-                    for k in range(2):
-                        setBlock(lv, x+j, y-1-k, z+i, 9, 0)
-                    setBlock(lv, x+j, y-3, z+i, 1, 0)
+        maxy = y
+
+        if self.direction is 0:
+            for i in range(w): 
+                gety = Test(lv, x, y, z+i, h)
+                y = gety.build()
+                for j in range(d): 
+                    if (j==0 or j==d):
+                        for k in range(4):
+                            setBlock(lv, x+j, y-k, z+i, 1, 0)
+                    else:
+                        setBlock(lv, x+j, y-k, z+i, 0, 0)
+                        for k in range(2):
+                            setBlock(lv, x+j, y-1-k, z+i, 9, 0)
+                        setBlock(lv, x+j, y-3, z+i, 1, 0)
+                    for s in range(y+1, maxy):
+                        setBlock(lv, x+j, s, z+i, 0, 0)
         if self.direction is 1:
-            for j in range(d): 
-                if (j==0 or j==d):
-                    for k in range(4):
-                        setBlock(lv, x+i, y-k, z+j, 1, 0)
-                else:
-                    setBlock(lv, x+i, y-k, z+j, 0, 0)
-                    for k in range(2):
-                        setBlock(lv, x+i, y-1-k, z+j, 9, 0)
-                    setBlock(lv, x+i, y-3, z+j, 1, 0)
+            for i in range(w):
+                gety = Test(lv, x, y, z+i, h)
+                y = gety.build()
+                for j in range(d): 
+                    if (j==0 or j==d):
+                        for k in range(4):
+                            setBlock(lv, x+i, y-k, z+j, 1, 0)
+                    else:
+                        setBlock(lv, x+i, y-k, z+j, 0, 0)
+                        for k in range(2):
+                            setBlock(lv, x+i, y-1-k, z+j, 9, 0)
+                        setBlock(lv, x+i, y-3, z+j, 1, 0)
+                    for s in range(y+1, maxy):
+                        setBlock(lv, x+i, s, z+j, 0, 0)
