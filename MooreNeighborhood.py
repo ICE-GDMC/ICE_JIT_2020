3abb18ed70697961c0567abe065791efb0c5e37c
Moss J <nejiahao@gmail.com>
2020-07-14 13:15:16 +0900

    Upload all at once

--- MooreNeighborhood.py
+++ MooreNeighborhood.py
@@ -4,7 +4,7 @@ from functions import *
 
 
 class MooreNeighbor:
-    def __init__(self, height_map, x, y, z, threshold=0):
+    def __init__(self, height_map, x, y, z, one_x_area, one_z_area, threshold=0):
         self.checked_list = []
         self.uncheck_list = []
         self.threshold = threshold
@@ -12,6 +12,8 @@ class MooreNeighbor:
         self.start_x = x
         self.start_y = y
         self.start_z = z
+        self.one_x_area = one_x_area
+        self.one_z_area = one_z_area
         self.pos = [[-1, 0], [-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1],
                     [-1, 1]]  # search in the 4 directions of the cell S-W-N-E
         # self.stoppingSign = 0
@@ -24,9 +26,11 @@ class MooreNeighbor:
         for i in range(8):
             p_x = self.pos[i][0] + x
             p_z = self.pos[i][1] + z  # search in the 4 directions of the cell S-W-N-E
+            if p_x < self.one_x_area[0] or p_x >= self.one_x_area[1] or p_z < self.one_z_area[0] or p_z >= \
+                    self.one_z_area[1]:
+                continue
             p_y = self.height_map.getHeight(p_x, p_z)
-
-            if 0 <= p_y - self.start_y <= self.threshold:
+            if abs(p_y - self.start_y) <= self.threshold:
                 if (p_x, p_y, p_z) not in self.checked_list and (p_x, p_y, p_z) not in self.uncheck_list:
                     self.uncheck_list.append((p_x, p_y, p_z))
         self.checked_list.append((x, y, z))
